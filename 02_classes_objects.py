05fc07433fc305cabed5e263f126b47c1512b551
Adrian Constantin Antochi <adrian.c.antochi@gmail.com>
2026-09-09 17:34:02 +0300

    added basic class

--- 02_classes_objects.py
+++ 02_classes_objects.py
@@ -1,20 +1,28 @@
 # self == this, in java sau javascript.
+# all methods surrounded by __, like __str__ are Dunder methods.
 
 class User:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
+        self.nationality = "Romanian"
 
     def say_hello(self):
         print(self.name + " says hi!")
 
+    def __str__(self):
+        return "" + self.name + " " + str(self.age) + ", " + self.nationality
 
-sonia = User("Sonia")
-dragos = User("Dragos")
+# folosind User() intializam o instanta a clasei User.
+sonia = User("Sonia", 30)
+dragos = User("Dragos", 35)
 
 print(sonia)
 print(dragos)
 
 print(sonia.name)
+sonia.say_hello()
+
 
 
 
